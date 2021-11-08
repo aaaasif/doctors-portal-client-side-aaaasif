b90ebfbfbfdde24ed1a8a121f7c884c710e24aff
aaaasif <alexasif4@gmail.com>
2021-11-08 14:23:26 +0600

    dashboard content create

---
+++ src/Pages/Dashboard/Appointments/Appointments.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import useAuth from '../../../hooks/useAuth'
+
+const Appointments = () => {
+    const { user } = useAuth();
+    const [appointments, setAppointments] = useState([]);
+    
+    useEffect( ()=> {
+        const url = ` `
+        fetch('')
+    }, [])
+
+    return (
+        <div>
+            <h2>Appointments</h2>
+        </div>
+    );
+};
+
+export default Appointments;
--- src/Pages/Dashboard/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard/Dashboard.js
@@ -15,6 +15,10 @@ import MailIcon from '@mui/icons-material/Mail';
 import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import Grid from '@mui/material/Grid';
+import Calendar from '../../Shared/Calendar/Calendar';
+import Appointments from '../Appointments/Appointments';
+
 
 const drawerWidth = 200;
 
@@ -108,7 +112,14 @@ function Dashboard(props) {
       >
         <Toolbar />
         <Typography paragraph>
-            Dashboard here
+            <Grid container spacing={2}>
+                <Grid item xs={8}>
+                  <Calendar></Calendar>
+                </Grid>
+                <Grid item xs={4}>
+                  <Appointments></Appointments>
+                </Grid>
+            </Grid>
         </Typography>
       </Box>
     </Box>
